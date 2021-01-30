87a82f11cb363805eb388b1a1f2ff105ba85e8cc
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-01-30 23:57:32 +0800

    CRUD operations added. iteration 2 done

--- routes/places.js
+++ routes/places.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const Place = require('./../models/place');
+const express = require("express");
+const Place = require("./../models/place");
 
 const router = express.Router();
 
 // Handle GET request for website root
 
 //Display all Places
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   Place.find()
     .then((places) => {
-      res.render('places/index', { places });
+      res.render("places/index", { places });
+      //res.render("index", { title: "Hello World!" });
     })
     .catch((error) => {
       next(error);
@@ -17,21 +18,21 @@ router.get('/', (req, res, next) => {
 });
 
 //Add Places
-router.get('/create', (req, res, next) => {
-  res.render('places/create');
+router.get("/create", (req, res, next) => {
+  res.render("places/create");
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   const data = req.body;
-  const places = new Place({
+  const place = new Place({
     name: data.name,
     type: data.type
   });
   place
     .save()
     .then((place) => {
-      console.log('Place created');
-      res.redirect('places');
+      console.log("Place created");
+      res.redirect("/places");
     })
     .catch((error) => {
       next(error);
@@ -40,11 +41,11 @@ router.post('/', (req, res, next) => {
 
 //Display single Place
 
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   const id = req.params.id;
   Place.findById(id)
     .then((places) => {
-      res.render('places/show', { places });
+      res.render("places/show", { places });
     })
     .catch((error) => {
       next(error);
@@ -53,38 +54,38 @@ router.get('/:id', (req, res, next) => {
 
 //Delete Place
 
-router.post('/:id/delete', (req, res, next) => {
+router.post("/:id/delete", (req, res, next) => {
   const id = req.params.id;
   Place.findByIdAndRemove(id)
     .then(() => {
-      res.redirect('/places');
+      res.redirect("/places");
     })
     .catch((error) => {
       next(error);
     });
 });
 
-//Iteration 6 (Bonus): Editing Places
+//Editing Places
 
-router.get('/:id/edit', (req, res, next) => {
+router.get("/:id/edit", (req, res, next) => {
   const id = req.params.id;
   Place.findById(id)
     .then((places) => {
-      res.render('places/edit', { places });
+      res.render("places/edit", { places });
     })
     .catch((error) => {
       next(error);
     });
 });
 
-router.post('/:id', (req, res, next) => {
+router.post("/:id", (req, res, next) => {
   const id = req.params.id;
   const name = req.body.name;
-  const occupation = req.body.type;
+  const type = req.body.type;
   Place.findByIdAndUpdate(id, { name, type })
     .then((places) => {
-      console.log('Place edited');
-      res.redirect('/places');
+      console.log("Place edited");
+      res.redirect("/places");
     })
     .catch((err) => {
       next(err);
